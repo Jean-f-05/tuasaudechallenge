2e444c47fd8e74e7749c1cb7db5268cb172c99e4
John Francis <jean_f_05@hotmail.com>
2022-06-26 20:48:52 +0100

    added markups to switch statement

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+import { light, moderate, serious } from "./markups/markups.js";
 const form = document.querySelector("form");
+const main = document.querySelector("#main");
 
 function isEmpty(obj) {
   return Object.keys(obj).length === 0;
@@ -11,17 +13,21 @@ function handleSubmit(e) {
 
   if (isEmpty(formProps)) {
     console.log("NOTHING");
+    main.insertAdjacentHTML("beforeend", light);
   } else {
     switch (true) {
       case Object.keys(formProps).length <= 3:
         console.log("SOME");
+        main.insertAdjacentHTML("beforeend", moderate);
         break;
       case Object.keys(formProps).length > 3:
         console.log("HIGH");
+        main.insertAdjacentHTML("beforeend", serious);
         break;
     }
 
     console.log(formProps);
+    console.log(light);
   }
 }
 
